0b286c0066c9709185b0a941e9eb05be16d14dfe
AbhishekMondal1 <Abhishekmondal.official@gmail.com>
2022-02-01 22:09:11 +0530

    add download audio 🎵 api, improve video ▶ api

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const ytdl = require("ytdl-core");
 const express = require("express");
 const fs = require("fs");
+const cors = require("cors");
 const PORT = 5000;
 
 const app = express();
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Welcome to Youtube downloader. 📁");
@@ -11,18 +14,50 @@ app.get("/", (req, res) => {
 
 const info = async(url) => {
     const videoInfo = await ytdl.getBasicInfo(url);
-    console.log('data');
+    console.log(videoInfo);
+    console.log('fetched info 🗒️');
     return videoInfo;
 }
 
-app.get("/download-video", (req, res) => {
+app.get("/download-audio", async(req, res) => {
     try {
         const url = req.query.url;
         if (!ytdl.validateURL(url)) {
             return res.sendStatus(400);
         }
-        info(url);
-        ytdl(url, { format: 'mp4', }).pipe(res);
+        let title = 'audio';
+        const videoInfo = await info(url);
+        if (videoInfo) {
+            title = videoInfo.videoDetails.title;
+            console.log(title);
+        }
+        res.header("Content-Disposition", `attachment; filename="${title}.mp3`);
+        const file = ytdl(url, { format: 'mp3', filter: 'audioonly' }).pipe(res);
+        file.on('finish', () => {
+            console.log('file send successfully 💯');
+        })
+    } catch (error) {
+        console.log(`🚫 ${error}`);
+    }
+})
+
+app.get("/download-video", async(req, res) => {
+    try {
+        const url = req.query.url;
+        if (!ytdl.validateURL(url)) {
+            return res.sendStatus(400);
+        }
+        let title = 'video';
+        const videoInfo = await info(url);
+        if (videoInfo) {
+            title = videoInfo.videoDetails.title;
+            console.log(title);
+        }
+        res.header("Content-Disposition", `attachment; filename="${title}.mp4"`)
+        const file = ytdl(url, { format: 'mp4', quality: '22', }).pipe(res);
+        file.on('finish', () => {
+            console.log('file send successfully 💯');
+        })
     } catch (error) {
         console.log(`🚫 ${error}`);
     }

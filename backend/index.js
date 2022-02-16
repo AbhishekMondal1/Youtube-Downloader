cb77e1d459f2fc324c14c13392ca6d68eb7f777c
AbhishekMondal1 <Abhishekmondal.official@gmail.com>
2022-02-16 20:58:27 +0530

    add server API 🔗️ to store and serve videos 💿️

--- backend/index.js
+++ backend/index.js
@@ -1,66 +1,30 @@
-const ytdl = require("ytdl-core");
 const express = require("express");
-const fs = require("fs");
 const cors = require("cors");
-const PORT = 5000;
-
+const multer = require("multer");
 const app = express();
+const PORT = process.env.PORT || 8000;
 
+require("dotenv").config();
 app.use(cors());
 
+const fileStorageEngine = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, process.env.STORAGE_PATH);
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+const upload = multer({ storage: fileStorageEngine });
+
 app.get("/", (req, res) => {
-    res.send("Welcome to Youtube downloader. 📁");
-})
+    res.send("Hello");
+});
 
-const info = async(url) => {
-    const videoInfo = await ytdl.getBasicInfo(url);
-    console.log(videoInfo);
-    console.log('fetched info 🗒️');
-    return videoInfo;
-}
+app.post("/uploadfile", upload.single("singlefile"), (req, res) => {
+    res.send(req.file.filename);
+});
 
-app.get("/download-audio", async(req, res) => {
-    try {
-        const url = req.query.url;
-        if (!ytdl.validateURL(url)) {
-            return res.sendStatus(400);
-        }
-        let title = 'audio';
-        const videoInfo = await info(url);
-        if (videoInfo) {
-            title = videoInfo.videoDetails.title;
-            console.log(title);
-        }
-        res.header("Content-Disposition", `attachment; filename="${title}.mp3`);
-        const file = ytdl(url, { format: 'mp3', filter: 'audioonly' }).pipe(res);
-        file.on('finish', () => {
-            console.log('file send successfully 💯');
-        })
-    } catch (error) {
-        console.log(`🚫 ${error}`);
-    }
-})
-
-app.get("/download-video", async(req, res) => {
-    try {
-        const url = req.query.url;
-        if (!ytdl.validateURL(url)) {
-            return res.sendStatus(400);
-        }
-        let title = 'video';
-        const videoInfo = await info(url);
-        if (videoInfo) {
-            title = videoInfo.videoDetails.title;
-            console.log(title);
-        }
-        res.header("Content-Disposition", `attachment; filename="${title}.mp4"`)
-        const file = ytdl(url, { format: 'mp4', quality: '22', }).pipe(res);
-        file.on('finish', () => {
-            console.log('file send successfully 💯');
-        })
-    } catch (error) {
-        console.log(`🚫 ${error}`);
-    }
-
-})
-app.listen(PORT, () => { console.log(`🚀 server running at port: ${PORT} ✅`) })
+app.listen(PORT, () => {
+    console.log(`server running at ${PORT}`);
+});

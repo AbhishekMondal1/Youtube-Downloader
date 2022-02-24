4ef05cbf9f294a86da58350a9f223cd372691435
AbhishekMondal1 <abhishekmondal.official@gmail.com>
2022-02-24 23:59:40 +0530

    add valid URL check in API ✔

--- client/pages/api/videodetails.js
+++ client/pages/api/videodetails.js
@@ -4,60 +4,62 @@ import { idvalidator } from "../../middlewares/idvalidator";
 import { idSchema } from "../../schema/idSchema";
 
 const getResolutions = (formats) =>
-    chain(formats)
+  chain(formats)
     .filter("height")
     .map("height")
     .uniq()
     .orderBy(null, "desc")
     .value();
 
-const handler = (req, res) => {
-    const { id } = req.query;
-    console.log(id);
+const handler = async (req, res) => {
+  const { id } = req.query;
+  console.log(id);
 
-    ytdl
-        .getInfo(id)
-        .then(({ videoDetails, formats }) => {
-            const { title, thumbnails, author } = videoDetails;
-            const thumbnailURL = last(thumbnails).url;
-            const resolutions = getResolutions(formats);
-            console.log(title);
-            let allFormats = [];
-            let audioFormats = [];
-            resolutions.forEach((resolutionVal) => {
-                formats.forEach((formatVal) => {
-                    if (
-                        formatVal.height == resolutionVal &&
-                        (formatVal.codecs ? formatVal.codecs.startsWith("avc1") : false) &&
-                        formatVal.contentLength
-                    )
-                        allFormats.push({
-                            resolutions: `${resolutionVal}`,
-                            duration: `${formatVal.approxDurationMs}`,
-                            length: `${formatVal.contentLength}`,
-                        });
-                });
-            });
+  try {
+    const videoInfo = await ytdl.getInfo(id);
+    const { videoDetails, formats } = videoInfo;
+    const { title, thumbnails, author } = videoDetails;
+    const thumbnailURL = last(thumbnails).url;
+    const resolutions = getResolutions(formats);
+    console.log(title);
+    let allFormats = [];
+    let audioFormats = [];
+    resolutions.forEach((resolutionVal) => {
+      formats.forEach((formatVal) => {
+        if (
+          formatVal.height == resolutionVal &&
+          (formatVal.codecs ? formatVal.codecs.startsWith("avc1") : false) &&
+          formatVal.contentLength
+        )
+          allFormats.push({
+            resolutions: `${resolutionVal}`,
+            duration: `${formatVal.approxDurationMs}`,
+            length: `${formatVal.contentLength}`,
+          });
+      });
+    });
 
-            const key = "resolutions";
+    const key = "resolutions";
 
-            const videoFormats = [
-                ...new Map(allFormats.map((item) => [item[key], item])).values(),
-            ];
+    const videoFormats = [
+      ...new Map(allFormats.map((item) => [item[key], item])).values(),
+    ];
 
-            formats.forEach((formatVal) => {
-                if (formatVal.hasAudio === true && formatVal.hasVideo === false) {
-                    audioFormats.push({
-                        quality: `${formatVal.audioBitrate}`,
-                        duration: `${formatVal.approxDurationMs}`,
-                        length: `${formatVal.contentLength}`,
-                    });
-                }
-            });
+    formats.forEach((formatVal) => {
+      if (formatVal.hasAudio === true && formatVal.hasVideo === false) {
+        audioFormats.push({
+          quality: `${formatVal.audioBitrate}`,
+          duration: `${formatVal.approxDurationMs}`,
+          length: `${formatVal.contentLength}`,
+        });
+      }
+    });
 
-            res.json({ title, thumbnailURL, videoFormats, audioFormats, author });
-        })
-        .catch((err) => console.log(err));
+    res.json({ title, thumbnailURL, videoFormats, audioFormats, author });
+  } catch (err) {
+    console.log(err);
+    if (err) res.status(400).json({ validurl: "false" });
+  }
 };
 
 export default idvalidator(idSchema, handler);
